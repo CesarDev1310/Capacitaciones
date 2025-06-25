a4f177eba34a9f58572d287aa88214dc086c268f
Cesar Mantilla <nmantillaq@gmail.com>
2025-06-24 21:09:10 -0500

    Clase 24-06

--- js/bandeja.js
+++ js/bandeja.js
@@ -1,6 +1,22 @@
-
-
-
+let miTabla = new DataTable("#miTabla", {
+  language: {
+    processing: "Procesando...",
+    lengthMenu: "Mostrar _MENU_ registros",
+    zeroRecords: "No se encontraron resultados",
+    emptyTable: "Ningún dato disponible en esta tabla",
+    infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+    infoFiltered: "(filtrado de un total de _MAX_ registros)",
+    search: "Buscar:",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    },
+    info: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+  },
+});
 
 let listaTipos = [
   {
@@ -79,22 +95,56 @@ let listaCursos = [
   },
 ];
 
+/*
+listaCursos.forEach((e) => {
+  let botoncito = "<button class='btn btn-sm btn-success' >Ver</button>";
 
+  if (e.cod_item == "C00002") {
+    botoncito =
+      "<button class='btn btn-sm btn-success' >Ver 1</button> <button class='btn btn-sm btn-success' >Ver 2</button>";
+  }
 
-let dataDemo = [
-  ["C00001", "Habilidades Blandas", listaTipos.find(x=> x.codTipo == "02").nombre],
-  ["C00002", "Ingles", listaTipos.find(x=> x.codTipo == "03").nombre],
-  ["C00003", "Chino Mandarin", listaTipos.find(x=> x.codTipo == "04").nombre]
-];
-
-let miTabla = new DataTable('#miTabla', {
-  language : {
-    url: '//cdn.datatables.net/plug-ins/2.3.2/i18n/es-ES.json',
-  },
-  columns:[
-    {title: "Código"},
-    {title: "Nombre Curso/Programa"},
-    {title: "Tipo"}
-  ],
-  data: dataDemo
+  miTabla.row
+    .add([
+      e.cod_item,
+      e.nombre,
+      listaTipos.find((x) => x.codTipo == e.codTipo).nombre,
+      botoncito,
+    ])
+    .draw(true);
+  miTabla.draw();
 });
+*/
+
+function generarTabla(v_arr01) {
+
+  v_arr01.forEach((e) => {
+    let botoncito = "<button class='btn btn-sm btn-success' >Ver</button>";
+
+    if (e.codTipo == "01") {
+      botoncito =
+        "<button class='btn btn-sm btn-success' >Ver</button>" +
+        '<button class="btn btn-sm btn-success" onclick="verCursos("1321321")" >Cursos</button>';
+    }
+
+    miTabla.row
+      .add([
+        e.cod_item,
+        e.nombre,
+        listaTipos.find((x) => x.codTipo == e.codTipo).nombre,
+        botoncito,
+      ])
+      .draw(true);
+    miTabla.draw();
+  });
+}
+
+generarTabla(listaCursos);
+generarTabla(listaProgramas);
+
+
+function verCursos(valor){
+
+  console.log("Codigo recibido "+ valor);
+
+}
